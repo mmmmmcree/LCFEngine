6f653dba7be6805e319874c53edfb5c000feae67
MMMMMCREE <1349751341@qq.com>
2025-09-21 00:27:25 +0800

    replace VulkanFrambuffer.h with VulkanFramebufferObject.h

---
+++ RenderEngine/include/Vulkan/VulkanFramebufferObject.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <vulkan/vulkan.hpp>
+#include "VulkanImage.h"
+#include "VulkanCommandBufferObject.h"
+#include <optional>
+
+namespace lcf::render {
+    class VulkanContext;
+
+    struct VulkanFramebufferObjectCreateInfo;
+
+    class VulkanFramebufferObject
+    {
+        using Self = VulkanFramebufferObject;
+    public:
+        class Attachment : public VulkanAttachment
+        {
+            using Self = Attachment;
+        public:
+            using VulkanAttachment::VulkanAttachment;
+            Attachment(const VulkanAttachment & attachment) : VulkanAttachment(attachment) {}
+            Self & setClearColorValue(const vk::ClearColorValue & clear_value) noexcept { m_clear_value = clear_value; return *this; }
+            Self & setClearDepthStencilValue(const vk::ClearDepthStencilValue & clear_value) noexcept { m_clear_value = clear_value; return *this; }
+            Self & setLoadOp(vk::AttachmentLoadOp load_op) noexcept { m_load_op = load_op; return *this; }
+            Self & setStoreOp(vk::AttachmentStoreOp store_op) noexcept { m_store_op = store_op; return *this; }
+            Self & setResolveMode(vk::ResolveModeFlagBits resolve_mode) noexcept { m_resolve_mode = resolve_mode; return *this; }
+            const vk::ClearValue & getClearValue() const noexcept { return m_clear_value; }
+            vk::AttachmentLoadOp getLoadOp() const noexcept { return m_load_op; }
+            vk::AttachmentStoreOp getStoreOp() const noexcept { return m_store_op; }
+            vk::ResolveModeFlagBits getResolveMode() const noexcept { return m_resolve_mode; }
+        private: 
+            vk::ClearValue m_clear_value = {};
+            vk::AttachmentLoadOp m_load_op = vk::AttachmentLoadOp::eClear;
+            vk::AttachmentStoreOp m_store_op = vk::AttachmentStoreOp::eStore;
+            vk::ResolveModeFlagBits m_resolve_mode = vk::ResolveModeFlagBits::eNone;
+        };
+        using OptionalAttachment = std::optional<Attachment>;
+        using AttachmentList = std::vector<Attachment>;
+        VulkanFramebufferObject() = default;
+        bool create(VulkanContext * context_p, const VulkanFramebufferObjectCreateInfo & create_info);
+        Self & addColorAttachment(const Attachment & attachment);
+        Self & setExtent(vk::Extent2D extent) noexcept { m_extent = std::min(m_max_extent, extent); return *this; }
+        Attachment & getMainColorAttachment() noexcept { return m_color_attachments.front(); }
+        OptionalAttachment getColorAttachment(uint32_t index) noexcept { return m_color_attachments.size() > index? std::make_optional(m_color_attachments[index]) : std::nullopt; }
+        OptionalAttachment getDepthStencilAttachment() noexcept { return m_depth_stencil_attachment; }
+        OptionalAttachment getMSAAResolveAttachment() noexcept { return m_msaa_resolve_attachment; }
+        void beginRendering(VulkanCommandBufferObject * cmd_p);
+        void endRendering(VulkanCommandBufferObject * cmd_p);
+    private:
+        vk::Extent2D m_max_extent = {-1u, -1u};
+        vk::Extent2D m_extent = {-1u, -1u};
+        vk::Format m_depth_stencil_format = vk::Format::eUndefined;
+        AttachmentList m_color_attachments;
+        OptionalAttachment m_depth_stencil_attachment;
+        OptionalAttachment m_msaa_resolve_attachment;
+    };
+
+    struct VulkanFramebufferObjectCreateInfo
+    {
+        using Self = VulkanFramebufferObjectCreateInfo;
+        using FormatList = std::vector<vk::Format>;
+        VulkanFramebufferObjectCreateInfo(vk::Extent2D max_extent = {800u, 600u},
+            vk::SampleCountFlagBits sample_count = vk::SampleCountFlagBits::e1,
+            vk::Format depth_stencil_format = vk::Format::eUndefined,
+            vk::ResolveModeFlagBits resolve_mode = vk::ResolveModeFlagBits::eNone) :
+            m_max_extent(max_extent),
+            m_depth_stencil_format(depth_stencil_format),
+            m_resolve_mode(resolve_mode) {}
+        Self & setMaxExtent(vk::Extent2D max_extent) noexcept { m_max_extent = max_extent; return *this; }
+        Self & addColorFormat(vk::Format format) { m_color_formats.emplace_back(format); return *this; }
+        Self & setSampleCount(vk::SampleCountFlagBits sample_count) noexcept { m_sample_count = sample_count; return *this; }
+        Self & setDepthStencilFormat(vk::Format depth_stencil_format) noexcept { m_depth_stencil_format = depth_stencil_format; return *this; }
+        Self & setResolveMode(vk::ResolveModeFlagBits resolve_mode) noexcept { m_resolve_mode = resolve_mode; return *this; }
+        const vk::Extent2D & getMaxExtent() const noexcept { return m_max_extent; }
+        const FormatList & getColorFormats() const noexcept { return m_color_formats; }
+        vk::SampleCountFlagBits getSampleCount() const noexcept { return m_sample_count; }
+        bool hasDepthStencilFormat() const noexcept { return m_depth_stencil_format != vk::Format::eUndefined; }
+        const vk::Format & getDepthStencilFormat() const noexcept { return m_depth_stencil_format; }
+        bool isEnableMSAA() const noexcept { return m_resolve_mode != vk::ResolveModeFlagBits::eNone; }
+        const vk::ResolveModeFlagBits & getResolveMode() const noexcept { return m_resolve_mode; }
+    //- properties
+        vk::Extent2D m_max_extent;
+        FormatList m_color_formats;
+        vk::SampleCountFlagBits m_sample_count;
+        vk::Format m_depth_stencil_format;
+        vk::ResolveModeFlagBits m_resolve_mode;
+    };
+}
---
+++ RenderEngine/src/Vulkan/VulkanFramebufferObject.cpp
@@ -0,0 +1,113 @@
+#include "VulkanFramebufferObject.h"
+#include "error.h"
+#include "VulkanContext.h"
+
+using namespace lcf::render;
+
+bool VulkanFramebufferObject::create(VulkanContext *context_p, const VulkanFramebufferObjectCreateInfo &create_info)
+{
+    if (not context_p or not context_p->isCreated()) {
+        LCF_THROW_RUNTIME_ERROR("lcf::render::VulkanFrameBufferObject::create: Invalid context");
+        return false;
+    }
+    m_max_extent = create_info.getMaxExtent();
+    m_extent = std::min(m_max_extent, m_extent);
+    vk::ImageUsageFlags color_attachment_usage = vk::ImageUsageFlagBits::eColorAttachment;
+    if (create_info.isEnableMSAA()) {
+        color_attachment_usage |= vk::ImageUsageFlagBits::eTransferSrc | vk::ImageUsageFlagBits::eSampled;
+    }
+    for (auto color_format : create_info.getColorFormats()) {
+        auto image_sp = VulkanImage::makeShared();
+        image_sp->setImageType(vk::ImageType::e2D)
+            .setUsage(color_attachment_usage)
+            .setExtent({m_max_extent.width, m_max_extent.height, 1u})
+            .setSamples(create_info.getSampleCount())
+            .setFormat(color_format)
+            .create(context_p);
+        m_color_attachments.emplace_back(image_sp);
+    }
+    if (m_color_attachments.empty()) {
+        auto image_sp = VulkanImage::makeShared();
+        image_sp->setImageType(vk::ImageType::e2D)
+            .setUsage(vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eTransferSrc | vk::ImageUsageFlagBits::eTransferDst)
+            .setExtent({m_max_extent.width, m_max_extent.height, 1u})
+            .setSamples(vk::SampleCountFlagBits::e1)
+            .setFormat(vk::Format::eR8G8B8A8Srgb)
+            .create(context_p);
+        m_color_attachments.emplace_back(image_sp);
+    }
+    if (create_info.hasDepthStencilFormat()) {
+        auto image_sp = VulkanImage::makeShared();
+        image_sp->setImageType(vk::ImageType::e2D)
+            .setUsage(vk::ImageUsageFlagBits::eDepthStencilAttachment)
+            .setExtent({m_extent.width, m_extent.height, 1})
+            .setSamples(m_color_attachments.front().getImageSharedPointer()->getSamples())
+            .setFormat(create_info.getDepthStencilFormat())
+            .create(context_p);
+        m_depth_stencil_attachment.emplace(image_sp);
+        m_depth_stencil_attachment->setClearDepthStencilValue({1.0f, 0});
+    }
+    if (create_info.isEnableMSAA()) {
+        auto image_sp = VulkanImage::makeShared();
+        image_sp->setImageType(vk::ImageType::e2D)
+            .setUsage(vk::ImageUsageFlagBits::eTransferSrc | vk::ImageUsageFlagBits::eTransferDst | vk::ImageUsageFlagBits::eColorAttachment)
+            .setFormat(m_color_attachments.front().getImageSharedPointer()->getFormat())
+            .setExtent({m_extent.width, m_extent.height, 1})
+            .setSamples(vk::SampleCountFlagBits::e1)
+            .create(context_p);
+        m_msaa_resolve_attachment.emplace(image_sp);
+        m_msaa_resolve_attachment->setResolveMode(create_info.getResolveMode());
+    }
+    return true;
+}
+
+VulkanFramebufferObject & VulkanFramebufferObject::addColorAttachment(const Attachment &attachment)
+{
+    m_color_attachments.emplace_back(attachment);
+    return *this;
+}
+
+void VulkanFramebufferObject::beginRendering(VulkanCommandBufferObject *cmd_p)
+{
+    std::vector<vk::RenderingAttachmentInfo> color_attachment_infos(m_color_attachments.size());
+    for (int i = 0; i < m_color_attachments.size(); ++i) {
+        auto & color_attachment = m_color_attachments[i];
+        color_attachment.setInitialLayout(vk::ImageLayout::eUndefined)
+            .transitLayout(cmd_p, vk::ImageLayout::eColorAttachmentOptimal);
+        color_attachment_infos[i].setImageLayout(vk::ImageLayout::eColorAttachmentOptimal)
+            .setImageView(color_attachment.getImageView())
+            .setLoadOp(color_attachment.getLoadOp())
+            .setStoreOp(color_attachment.getStoreOp())
+            .setClearValue(color_attachment.getClearValue());
+    }
+    vk::RenderingAttachmentInfo depth_stencil_attachment_info;
+    if (m_depth_stencil_attachment) {
+        auto & depth_stencil_attachment = *m_depth_stencil_attachment;
+        depth_stencil_attachment.setInitialLayout(vk::ImageLayout::eUndefined)
+            .transitLayout(cmd_p, vk::ImageLayout::eDepthStencilAttachmentOptimal);
+        depth_stencil_attachment_info.setImageLayout(vk::ImageLayout::eDepthStencilAttachmentOptimal)
+            .setImageView(depth_stencil_attachment.getImageView())
+            .setLoadOp(depth_stencil_attachment.getLoadOp())
+            .setStoreOp(depth_stencil_attachment.getStoreOp())
+            .setClearValue(depth_stencil_attachment.getClearValue());
+    }
+    if (m_msaa_resolve_attachment) {
+        auto & msaa_resolve_attachment = *m_msaa_resolve_attachment;
+        msaa_resolve_attachment.setInitialLayout(vk::ImageLayout::eUndefined)
+            .transitLayout(cmd_p, vk::ImageLayout::eColorAttachmentOptimal);
+        color_attachment_infos[0].setResolveImageLayout(vk::ImageLayout::eColorAttachmentOptimal)
+            .setResolveImageView(msaa_resolve_attachment.getImageView())
+            .setResolveMode(msaa_resolve_attachment.getResolveMode());
+    }
+    vk::RenderingInfo rendering_info;
+    rendering_info.setRenderArea({ { 0, 0 }, m_extent })
+        .setLayerCount(1)
+        .setColorAttachments(color_attachment_infos)
+        .setPDepthAttachment(&depth_stencil_attachment_info);
+    cmd_p->beginRendering(rendering_info);
+}
+
+void VulkanFramebufferObject::endRendering(VulkanCommandBufferObject *cmd_p)
+{
+    cmd_p->endRendering();
+}
