3da8b4c3fa02baa3a1b540d4ed0c87f4e6d8cb21
MMMMMCREE <1349751341@qq.com>
2025-10-04 22:57:49 +0800

    add intergral_span_convertible_c

--- utilities/include/concepts/span_convertible_concept.h
+++ utilities/include/concepts/span_convertible_concept.h
@@ -3,7 +3,11 @@
 #include <ranges>
 
 namespace lcf {
-    template<typename T>
+    template <typename T>
     concept span_convertible_c = std::ranges::contiguous_range<std::remove_cvref_t<T>> and
         std::ranges::sized_range<std::remove_cvref_t<T>>;
+
+    template <typename T>
+    concept integral_span_convertible_c = span_convertible_c<T> and
+        std::is_integral_v<std::ranges::range_value_t<std::remove_cvref_t<T>>>;
 }
