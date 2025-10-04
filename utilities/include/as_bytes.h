dc79e375615dcf180498695e053c8b85cd2ec741
MMMMMCREE <1349751341@qq.com>
2025-10-04 22:54:39 +0800

    distinguish const type and non const type

--- utilities/include/as_bytes.h
+++ utilities/include/as_bytes.h
@@ -6,21 +6,31 @@
 namespace lcf {
     using ByteView = std::span<const std::byte>;
 
+    template <typename T, std::size_t Extent>
+    auto span_as_bytes(std::span<T, Extent> span)
+    {
+        if constexpr (std::is_const_v<T>) {
+            return std::as_bytes(span);
+        } else {
+            return std::as_writable_bytes(span);
+        }
+    }
+
     template <span_convertible_c SpanConvertible>
     auto as_bytes(SpanConvertible & data)
     {
-        return std::as_bytes(std::span(data));
+        return span_as_bytes(std::span(data));
     }
 
     template <typename T>
-    auto as_bytes_from_ptr(const T * data_p, size_t size_in_bytes)
+    auto as_bytes_from_ptr(T * data_p, size_t element_count)
     {
-        return std::as_bytes(std::span(data_p, size_in_bytes));
+        return span_as_bytes(std::span(data_p, element_count));
     }
 
     template <typename T>
     auto as_bytes_from_value(T & data)
     {
-        return std::as_bytes(std::span(&data, 1));
+        return span_as_bytes(std::span(&data, 1));
     }
 }
