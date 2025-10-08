f3a774d7bd89a1d0e83e657dbf6ebcb55a60423f
MMMMMCREE <1349751341@qq.com>
2025-10-08 23:45:26 +0800

    Added handling logic for when the passed pointer is of type void *

--- utilities/include/as_bytes.h
+++ utilities/include/as_bytes.h
@@ -7,7 +7,7 @@ namespace lcf {
     using ByteView = std::span<const std::byte>;
 
     template <typename T, std::size_t Extent>
-    auto span_as_bytes(std::span<T, Extent> span)
+    constexpr auto span_as_bytes(std::span<T, Extent> span) noexcept
     {
         if constexpr (std::is_const_v<T>) {
             return std::as_bytes(span);
@@ -17,19 +17,24 @@ namespace lcf {
     }
 
     template <span_convertible_c SpanConvertible>
-    auto as_bytes(SpanConvertible & data)
+    constexpr auto as_bytes(SpanConvertible & data) noexcept
     {
         return span_as_bytes(std::span(data));
     }
 
     template <typename T>
-    auto as_bytes_from_ptr(T * data_p, size_t element_count)
+    constexpr auto as_bytes_from_ptr(T * data_p, size_t element_count) noexcept // element_count is size_in_bytes if T is void
     {
-        return span_as_bytes(std::span(data_p, element_count));
+        if constexpr (std::is_void_v<std::remove_cvref_t<T>>) {
+            using ByteType = std::conditional_t<std::is_const_v<T>, const std::byte, std::byte>;
+            return span_as_bytes(std::span(static_cast<ByteType *>(data_p), element_count));
+        } else {
+            return span_as_bytes(std::span(data_p, element_count));
+        }
     }
 
     template <typename T>
-    auto as_bytes_from_value(T & data)
+    constexpr auto as_bytes_from_value(T & data) noexcept
     {
         return span_as_bytes(std::span(&data, 1));
     }
