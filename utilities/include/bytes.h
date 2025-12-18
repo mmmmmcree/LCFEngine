6325e29820cf2da0280df0a1e2a4d570a1dcabe6
MMMMMCREE <1349751341@qq.com>
2025-12-18 19:05:15 +0800

    rename as_bytes.h to bytes.h

--- utilities/include/bytes.h
+++ utilities/include/bytes.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "concepts/span_convertible_concept.h"
+#include "concepts/standard_layout_concept.h"
 #include <span>
 
 namespace lcf {
     using ByteView = std::span<const std::byte>;
+    using WritableByteView = std::span<std::byte>;
 
-    template <typename T, std::size_t Extent>
+    template <standard_layout_c T, std::size_t Extent>
     constexpr auto span_as_bytes(std::span<T, Extent> span) noexcept
     {
         if constexpr (std::is_const_v<T>) {
@@ -22,7 +24,7 @@ namespace lcf {
         return span_as_bytes(std::span(data));
     }
 
-    template <typename T>
+    template <standard_layout_c T>
     constexpr auto as_bytes_from_ptr(T * data_p, size_t element_count) noexcept // element_count is size_in_bytes if T is void
     {
         if constexpr (std::is_void_v<std::remove_cvref_t<T>>) {
@@ -33,9 +35,23 @@ namespace lcf {
         }
     }
 
-    template <typename T>
+    template <standard_layout_c T>
     constexpr auto as_bytes_from_value(T & data) noexcept
     {
         return span_as_bytes(std::span(&data, 1));
+    }
+
+    template <standard_layout_c T>
+    std::span<const T> bytes_as_span(ByteView bytes) noexcept
+    {
+        assert(bytes.size() % sizeof(T) == 0);
+        return std::span<const T>(reinterpret_cast<const T *>(bytes.data()), bytes.size() / sizeof(T));
+    }
+
+    template <standard_layout_c T>
+    std::span<T> bytes_as_span(WritableByteView bytes) noexcept
+    {
+        assert(bytes.size() % sizeof(T) == 0);
+        return std::span<T>(reinterpret_cast<T *>(bytes.data()), bytes.size() / sizeof(T));
     }
 }
