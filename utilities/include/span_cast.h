feb1cb7947ad6ff84ba0ccb8fa31623509a7dfd3
MMMMMCREE <1349751341@qq.com>
2026-01-27 21:44:59 +0800

    span cast

--- utilities/include/span_cast.h
+++ utilities/include/span_cast.h
@@ -1,14 +1,32 @@
 #pragma once
 
-#include "concepts/span_convertible_concept.h"
+#include "lcf_type_traits.h"
+#include <ranges>
+#include <span>
 
 namespace lcf {
-    template<typename T, span_convertible_c SpanConvertible>
-    auto span_cast(SpanConvertible & span_convertible) noexcept
+    template <typename Dst, typename Src>
+    requires is_pointer_type_convertible_v<std::remove_cv_t<Src>, std::remove_cv_t<Dst>>
+    auto pointer_type_cast(Src * src) noexcept
     {
-        auto span = std::span(span_convertible);
-        using SrcType = typename decltype(span)::element_type;
-        using DstType = std::conditional_t<std::is_const_v<SrcType>, const T, T>;
-        return std::span<DstType>(reinterpret_cast<DstType *>(span.data()), span.size_bytes() / sizeof(DstType));
+        using result_type = std::conditional_t<std::is_const_v<Src>,
+            std::conditional_t<std::is_volatile_v<Src>, const volatile Dst*, const Dst*>,
+            std::conditional_t<std::is_volatile_v<Src>, volatile Dst*, Dst*>>;
+        return reinterpret_cast<result_type>(src);
+    }
+}
+
+namespace lcf {
+    template <typename T, std::ranges::contiguous_range Range>
+    requires is_pointer_type_convertible_v<std::ranges::range_value_t<Range>, T> or std::is_same_v<std::ranges::range_value_t<Range>, T>
+    auto span_cast(Range && range) noexcept
+    {
+        auto span = std::span(std::forward<Range>(range));
+        using Src = typename decltype(span)::element_type;
+        using Dst = std::conditional_t<std::is_const_v<Src>, const T, T>;
+        if constexpr (std::is_same_v<std::ranges::range_value_t<Range>, T>) { return span; }
+        else {
+            return std::span<Dst>(pointer_type_cast<Dst>(span.data()), span.size_bytes() / sizeof(Dst));
+        }
     }
 }
