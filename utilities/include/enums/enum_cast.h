7c84e7efe0e1b4e741affd569514bd4f2387c7eb
MMMMMCREE <1349751341@qq.com>
2026-01-29 18:01:20 +0800

    define mapping between two enum types, instead of  multiple enum types.

--- utilities/include/enums/enum_cast.h
+++ utilities/include/enums/enum_cast.h
@@ -4,22 +4,20 @@
 #include "enum_flags.h"
 #include <tuple>
 
+
 namespace lcf {
-    template <enum_c Enum>
-    struct enum_category_traits
+    template <enum_c Dst, enum_c Src>
+    struct enum_mapping_traits
     {
-        using category_type = std::tuple<>;
-        constexpr static category_type mappings[] = {{}};
+        using mapping_type = std::tuple<Src, Dst>;
+        static constexpr size_t N = sizeof(enum_mapping_traits<Src, Dst>::mappings) / sizeof(mapping_type);
+        static constexpr mapping_type mappings[N] = enum_mapping_traits<Src, Dst>::mappings;
     };
 
-    template <enum_c Enum>
-    using enum_category_t = typename enum_category_traits<Enum>::category_type;
-
     template <enum_c Dst, enum_c Src>
-    requires std::same_as<enum_category_t<Dst>, enum_category_t<Src>>
     constexpr Dst enum_cast(Src src) noexcept
     {
-        for (const auto & mapping : enum_category_traits<Dst>::mappings) {
+        for (const auto & mapping : enum_mapping_traits<Dst, Src>::mappings) {
             if (std::get<Src>(mapping) == src) {
                 return std::get<Dst>(mapping);
             }
@@ -28,11 +26,10 @@ namespace lcf {
     }
 
     template <enum_flags_c Dst, enum_flags_c Src>
-    requires std::same_as<enum_category_t<Dst>, enum_category_t<Src>>
     constexpr Dst enum_flags_cast(Src src) noexcept
     {
         Dst dst {};
-        for (const auto & mapping : enum_category_traits<Dst>::mappings) {
+        for (const auto & mapping : enum_mapping_traits<Dst>::mappings) {
             if (static_cast<bool>(std::get<Src>(mapping) & src)) {
                 dst |= std::get<Dst>(mapping);
             }
@@ -41,31 +38,8 @@ namespace lcf {
     }
 }
 
-#include <boost/preprocessor.hpp>
-#include <concepts/same_as_concept.h>
-
-#define LCF_MAKE_ENUM_CATEGORY(enum_types, ...) \
-    LCF_DETAIL_MAKE_ENUM_CATEGORY_IMPL( \
-        BOOST_PP_TUPLE_TO_SEQ(enum_types), \
-        __VA_ARGS__ \
-    )
-
-#define LCF_DETAIL_MAKE_ENUM_CATEGORY_IMPL(enum_types_seq, ...) \
-    template <lcf::enum_c Enum> \
-    requires lcf::any_same_as_c< \
-        Enum, \
-        BOOST_PP_SEQ_ENUM(enum_types_seq) \
-    > \
-    struct lcf::enum_category_traits<Enum> \
-    { \
-        using category_type = std::tuple< \
-            BOOST_PP_SEQ_ENUM(enum_types_seq) \
-        >; \
-        constexpr static category_type mappings[] = { __VA_ARGS__ }; \
-    };
-
 /* Example usage:
-#include "enum_cast.h"
+#include "enums/enum_cast.h"
 
 namespace lib_a {
     enum class Color
@@ -86,35 +60,15 @@ namespace lib_b {
     };
 }
 
-namespace lib_C {
-    enum COLOR
-    {
-        red = 1 << 0,
-        green = 1 << 1,
-        blue = 1 << 2,
-    };
-}
-
-LCF_MAKE_ENUM_CATEGORY((lib_a::Color, lib_b::Color, lib_C::COLOR),
-    {lib_a::Color::eRed, lib_b::Color::RED, lib_C::red},
-    {lib_a::Color::eGreen, lib_b::Color::GREEN, lib_C::green},
-    {lib_a::Color::eBlue, lib_b::Color::BLUE, lib_C::blue}
-);
-
-// ! or without the macro:
-
-template <lcf::enum_c Enum>
-requires lcf::any_same_as_c<Enum, lib_a::Color, lib_b::Color, lib_C::COLOR>
-struct lcf::enum_category_traits<Enum>
+template <>
+struct lcf::enum_mapping_traits<lib_a::Color, lib_b::Color>
 {
-    using category_type = std::tuple<lib_a::Color, lib_b::Color, lib_C::COLOR>;
-    constexpr static category_type mappings[] = {
-        {lib_a::Color::eRed, lib_b::Color::RED, lib_C::red},
-        {lib_a::Color::eGreen, lib_b::Color::GREEN, lib_C::green},
-        {lib_a::Color::eBlue, lib_b::Color::BLUE, lib_C::blue},
+    constexpr static std::tuple<lib_a::Color, lib_b::Color> mappings[] = {
+        {lib_a::Color::eRed, lib_b::Color::RED},
+        {lib_a::Color::eGreen, lib_b::Color::GREEN},
+        {lib_a::Color::eBlue, lib_b::Color::BLUE},
     };
 };
-
 
 #include <iostream>
 
