2660d505cfcc7aa81c0d90bc25d52b99b329afdd
MMMMMCREE <1349751341@qq.com>
2026-02-10 00:26:30 +0800

    main test

--- examples/render_assets_test/src/main.cpp
+++ examples/render_assets_test/src/main.cpp
@@ -9,36 +9,20 @@ using namespace lcf;
 namespace stdr = std::ranges;
 
 #include "image/Image.h"
+#include "Vector.h"
 
 int main() {
-    Image image;
-    image.loadFromFile({"./assets/images/bk.jpg"});
-    image.resize(100, 100, ImageSampler::eNearest);
-    image.saveToFile("test.png");
+    lcf::Logger::init();
     
+    Registry registry;
 
-    // lcf::Logger::init();
-    
-    // Material material;
-    // auto segments = material.generateInterleavedSegments<glsl::std430::enum_value_type_mapping_t>(ShadingModel::eStandard);
-    // std::vector<float> material_params(segments.getUpperBoundInBytes() / sizeof(float));
-    // for (const auto & segment : segments) {
-    //     std::ranges::copy(segment.getDataView(), as_bytes(material_params).begin() + segment.getBeginOffsetInBytes());
-    // }
-
-    // for (auto v : material_params) {
-    //     std::cout << v << ' ';
-    // }
-
-    // Registry registry;
-
-    // ModelLoader loader;
-    // auto model_opt = loader.load("./assets/models/BarbieDodgePickup/scene.gltf");
-    // if (model_opt) {
-    //     auto & model = model_opt.value();
-    //     model.generateEntities(registry);
-    // } else {
-    //     lcf_log_error("Failed to load model");
-    // }
+    ModelLoader loader;
+    auto model_opt = loader.load("./assets/models/BarbieDodgePickup/scene.gltf");
+    if (model_opt) {
+        auto & model = model_opt.value();
+        model.generateEntities(registry);
+    } else {
+        lcf_log_error("Failed to load model");
+    }
     return 0;
 }
