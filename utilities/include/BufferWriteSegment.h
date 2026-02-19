e0f879ecd1a536f6e664a330028e1e0448e3f890
MMMMMCREE <1349751341@qq.com>
2026-02-19 22:23:42 +0800

    add move constructor

--- utilities/include/BufferWriteSegment.h
+++ utilities/include/BufferWriteSegment.h
@@ -11,10 +11,11 @@ namespace lcf {
         BufferWriteSegment() = default;
         BufferWriteSegment(ByteView bytes, size_t offset_in_bytes = 0u) noexcept :
             m_data(bytes), m_offset_in_bytes(offset_in_bytes) {}
-        BufferWriteSegment(const BufferWriteSegment& other) = default;
-        BufferWriteSegment(BufferWriteSegment&& other) noexcept = default;
-        BufferWriteSegment& operator=(const BufferWriteSegment& other) = default;
-        ByteView getDataView() const noexcept { return m_data; }
+        BufferWriteSegment(const BufferWriteSegment & other) = default;
+        BufferWriteSegment & operator=(const BufferWriteSegment & other) = default;
+        BufferWriteSegment(BufferWriteSegment && other) noexcept = default;
+        BufferWriteSegment & operator=(BufferWriteSegment && other) noexcept = default;
+        ByteView getDataSpan() const noexcept { return m_data; }
         const std::byte * getData() const noexcept { return m_data.data(); }
         size_t getSizeInBytes() const noexcept { return m_data.size_bytes(); }
         size_t getBeginOffsetInBytes() const noexcept { return m_offset_in_bytes; }
@@ -36,7 +37,7 @@ namespace lcf {
 }
 
 namespace lcf::impl {
-    template <typename Segments>
+    template <typename SegmentContainer>
     class BufferWriteSegmentsIMPL;
 
     template <>
@@ -46,10 +47,15 @@ namespace lcf::impl {
     public:
         using SegmentContainer = std::deque<BufferWriteSegment>;
         BufferWriteSegmentsIMPL() = default;
+        ~BufferWriteSegmentsIMPL() noexcept = default;
+        BufferWriteSegmentsIMPL(const Self &) = default;
+        BufferWriteSegmentsIMPL(Self &&) noexcept = default;
+        Self & operator=(const Self &) = default;
+        Self & operator=(Self &&) noexcept = default;
         bool isValid() const noexcept { return m_write_segment_lower_bound < m_write_segment_upper_bound; }
-        Self & add(const BufferWriteSegment& segment) noexcept
+        Self & add(const BufferWriteSegment & segment) noexcept
         {
-            return this->add(segment.getDataView(), segment.getBeginOffsetInBytes());
+            return this->add(segment.getDataSpan(), segment.getBeginOffsetInBytes());
         }
         Self & add(ByteView bytes, size_t offset_in_bytes) noexcept
         {
