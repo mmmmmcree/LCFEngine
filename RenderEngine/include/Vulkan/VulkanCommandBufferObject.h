b3d205afa47bf42136b39d6df0348a7bf6d9f405
MMMMMCREE <1349751341@qq.com>
2026-03-22 18:24:12 +0800

    use ResourceLease to delay resource destruction

--- RenderEngine/include/Vulkan/VulkanCommandBufferObject.h
+++ RenderEngine/include/Vulkan/VulkanCommandBufferObject.h
@@ -2,8 +2,8 @@
 
 #include <vulkan/vulkan.hpp>
 #include "vulkan_fwd_decls.h"
-#include "VulkanSharableResource.h"
 #include "VulkanTimelineSemaphore.h"
+#include "resource_utils.h"
 #include <boost/container/vector.hpp>
 #include <boost/container/vector.hpp>
 #include <queue>
@@ -14,7 +14,7 @@ namespace lcf::render {
         using Self = VulkanCommandBufferObject;
     public:
         using SemaphoreSubmitInfoList = boost::container::vector<vk::SemaphoreSubmitInfo>;
-        using ResourceList =  boost::container::vector<VulkanSharableResource>;
+        using ResourceLeases = boost::container::vector<ResourceLease>;
         VulkanCommandBufferObject() = default;
         ~VulkanCommandBufferObject() noexcept;
         std::error_code create(VulkanContext * context_p, vk::QueueFlagBits queue_type);
@@ -25,13 +25,13 @@ namespace lcf::render {
         Self & addWaitSubmitInfo(const vk::SemaphoreSubmitInfo & wait_info) { m_wait_infos.emplace_back(wait_info); return *this; }
         Self & addSignalSubmitInfo(const vk::SemaphoreSubmitInfo & signal_info) { m_signal_infos.emplace_back(signal_info); return *this; }
         vk::SemaphoreSubmitInfo submit();
-        void acquireResource(VulkanSharableResource resource);
+        void acquireResourceLease(ResourceLease resource_lease);
     private:
         VulkanContext * m_context_p = nullptr;
         vk::QueueFlagBits m_queue_type;
         VulkanTimelineSemaphoreSharedPointer m_timeline_semaphore_sp;
         SemaphoreSubmitInfoList m_wait_infos;
         SemaphoreSubmitInfoList m_signal_infos;
-        ResourceList m_resources;
+        ResourceLeases m_resource_leases;
     };
 }
