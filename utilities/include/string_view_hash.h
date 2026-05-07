e984bfc183e92a9e8fa2b8afe315c6ef79ad87a9
MMMCREELIU-PC0\mmmcreeliu <mmmcreeliu.tencent.com>
2026-05-07 20:06:36 +0800

    distinguish ul and ull

--- utilities/include/string_view_hash.h
+++ utilities/include/string_view_hash.h
@@ -6,14 +6,14 @@ namespace lcf::detail {
     template <std::integral Int, typename Char>
     constexpr Int fnv1a_impl(const Char * str, size_t len, Int hash) noexcept
     {
-        constexpr Int c_prime = 0x100000001b3ULL;
+        constexpr Int k_prime = (sizeof(Int) == 8) ? 0x100000001b3ULL : 0x01000193UL;
         return len == 0 ? hash : fnv1a_impl(str + 1, len - 1, (hash ^ static_cast<Int>(*str)) * c_prime);
     }
 
     template <std::integral Int, typename Char>
     constexpr Int fnv1a(std::basic_string_view<Char> str_view) noexcept
     {
-        constexpr Int c_offset = 0xcbf29ce484222325ULL;
+        constexpr Int k_offset = (sizeof(Int) == 8) ? 0xcbf29ce484222325ULL : 0x811c9dc5UL;
         return detail::fnv1a_impl<Int, Char>(str_view.data(), str_view.size(), c_offset);
     }
 }
