4203ed698fe9d6bcea531e87d7150c5ce327200b
mmmmmcree <1349751341@qq.com>
2026-06-29 00:27:36 +0800

    has a cmd allocator

--- libs/vk_core/include/vk_core/context/QueueContext.h
+++ libs/vk_core/include/vk_core/context/QueueContext.h
@@ -1,30 +1,41 @@
 #pragma once
 
 #include <vulkan/vulkan.hpp>
+#include <queue>
+#include <vector>
 #include "vk_core/sync/TimelineSemaphore.h"
+#include "vk_core/command/details/CommandBufferAllocator.h"
 #include "resource_utils.h"
 
 namespace lcf::vkc {
 
+class CommandBufferProxy;
+
 class QueueContext
 {
     using Self = QueueContext;
+    using LeaseBatch = std::pair<uint64_t, std::vector<ResourceLease>>;
+    using LeaseBatchQueue = std::queue<LeaseBatch>;
 public:
     ~QueueContext() noexcept = default;
     QueueContext() = default;
     QueueContext(const Self &) = delete;
-    QueueContext(Self &&) = default;
+    QueueContext(Self &&) = delete;
     Self & operator=(const Self &) = delete;
-    Self & operator=(Self &&) = default;
+    Self & operator=(Self &&) = delete;
 public:
     std::error_code create(vk::Device device, uint32_t family_index, uint32_t queue_index) noexcept;
     const vk::Queue & getQueue() const noexcept { return m_queue; }
     const uint32_t & getFamilyIndex() const noexcept { return m_family_index; }
-    void acquireResourceLease(ResourceLease lease) noexcept;
+    void submit(CommandBufferProxy && cmd_proxy) noexcept;
+    void collectGarbage() noexcept;
 private:
+    vk::Device m_device;
     vk::Queue m_queue;
-    uint32_t m_family_index;
+    uint32_t m_family_index = 0u;
     TimelineSemaphore m_timeline;
+    details::CommandBufferAllocator m_cmd_allocator;
+    LeaseBatchQueue m_lease_batch_queue;
 };
 
 } // namespace lcf::vkc
--- libs/vk_core/src/context/QueueContext.cpp
+++ libs/vk_core/src/context/QueueContext.cpp
@@ -6,7 +6,21 @@ std::error_code QueueContext::create(vk::Device device, uint32_t family_index, u
 {
     m_family_index = family_index;
     m_queue = device.getQueue(family_index, queue_index);
-    return m_timeline.create(device);
+    if (auto ec = m_cmd_allocator.create(device, m_family_index, this)) { return ec; }
+    if (auto ec = m_timeline.create(device)) { return ec; }
+    return {}; 
+}
+
+
+void QueueContext::submit(CommandBufferProxy && cmd_proxy) noexcept
+{
+
+}
+
+
+void QueueContext::collectGarbage() noexcept
+{
+    
 }
 
 } // namespace lcf::vkc
