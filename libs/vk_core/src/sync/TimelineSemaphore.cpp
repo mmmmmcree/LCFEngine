14afb0dcb8806016ccf432b18450810426885942
MMMCREELIU-PC0\mmmcreeliu <mmmcreeliu.tencent.com>
2026-06-29 13:51:28 +0800

    update timeline interface

--- libs/vk_core/src/sync/TimelineSemaphore.cpp
+++ libs/vk_core/src/sync/TimelineSemaphore.cpp
@@ -1,11 +1,12 @@
 #include "vk_core/sync/TimelineSemaphore.h"
+#include <limits>
 
 using namespace lcf::vkc;
 
 TimelineSemaphore::TimelineSemaphore(Self && other) noexcept :
     m_device(std::exchange(other.m_device, nullptr)),
     m_semaphore(std::move(other.m_semaphore)),
-    m_target_value(std::exchange(other.m_target_value, 0))
+    m_target_timestamp(std::exchange(other.m_target_timestamp, 0))
 {
 }
 
@@ -14,19 +15,18 @@ auto TimelineSemaphore::operator=(Self && other) noexcept -> Self &
     if (this == &other) { return *this; }
     m_device = std::exchange(other.m_device, nullptr);
     m_semaphore = std::move(other.m_semaphore);
-    m_target_value = std::exchange(other.m_target_value, 0);
+    m_target_timestamp = std::exchange(other.m_target_timestamp, 0);
     return *this;
 }
 
 std::error_code TimelineSemaphore::create(vk::Device device)
 {
-    if (not device) { return std::make_error_code(std::errc::invalid_argument); }
     m_device = device;
     vk::StructureChain<
         vk::SemaphoreCreateInfo,
         vk::SemaphoreTypeCreateInfo> semaphore_info;
     semaphore_info.get<vk::SemaphoreTypeCreateInfo>().setSemaphoreType(vk::SemaphoreType::eTimeline)
-        .setInitialValue(m_target_value);
+        .setInitialValue(m_target_timestamp);
     try {
         m_semaphore = device.createSemaphoreUnique(semaphore_info.get());
     } catch (const vk::SystemError & e) {
@@ -35,24 +35,37 @@ std::error_code TimelineSemaphore::create(vk::Device device)
     return {};
 }
 
-std::error_code TimelineSemaphore::waitFor(uint64_t value) const noexcept
+std::error_code TimelineSemaphore::waitFor(uint64_t timestamp) const noexcept
 {
     vk::SemaphoreWaitInfo wait_info;
     wait_info.setSemaphores(m_semaphore.get())
-        .setPValues(&value);
-    vk::Result result = m_device.waitSemaphores(wait_info, std::numeric_limits<uint64_t>::max());
-    if (result != vk::Result::eSuccess) {
-        return std::make_error_code(std::errc::timed_out);
+        .setPValues(&timestamp);
+    try {
+        vk::Result result = m_device.waitSemaphores(wait_info, std::numeric_limits<uint64_t>::max());
+        if (result == vk::Result::eTimeout) { return std::make_error_code(std::errc::timed_out); }
+    } catch (const vk::SystemError & e) {
+        return e.code();
     }
     return {};
 }
 
-uint64_t TimelineSemaphore::getCurrentValue() const
+std::expected<uint64_t, std::error_code> TimelineSemaphore::getCurrentTimestamp() const noexcept
 {
-    return m_device.getSemaphoreCounterValue(m_semaphore.get());
+    uint64_t timestamp;
+    try {
+        timestamp = m_device.getSemaphoreCounterValue(m_semaphore.get());
+    } catch (const vk::SystemError & e) {
+        return std::unexpected(e.code());
+    }
+    return timestamp;
+}
+
+std::expected<bool, std::error_code> lcf::vkc::TimelineSemaphore::isTargetReached(uint64_t timestamp) const noexcept
+{
+    return this->getCurrentTimestamp().transform([timestamp](uint64_t current) { return timestamp <= current; });
 }
 
 vk::SemaphoreSubmitInfo TimelineSemaphore::generateSubmitInfo() const noexcept
 {
-    return vk::SemaphoreSubmitInfo(m_semaphore.get(), m_target_value);
+    return vk::SemaphoreSubmitInfo(m_semaphore.get(), m_target_timestamp);
 }
