ed180f276e2614e90da0e96b55401894993aad90
MMMMMCREE\13497 <1349751341@qq.com>
2026-07-15 19:42:37 +0800

    min->max

--- libs/vk_core/include/vk_core/command/CommandBufferAllocateInfo.h
+++ libs/vk_core/include/vk_core/command/CommandBufferAllocateInfo.h
@@ -11,7 +11,7 @@ class CommandBufferAllocateInfo
 public:
     Self & addUsageFlags(CommandBufferUsageFlags flags) noexcept { m_usage_flags |= flags; return *this; }
     Self & setLevel(vk::CommandBufferLevel level) noexcept { m_level = level; return *this; }
-    Self & setCount(uint32_t count) noexcept { m_count = std::min(count, 1u); return *this; }
+    Self & setCount(uint32_t count) noexcept { m_count = std::max(count, 1u); return *this; }
     CommandBufferUsageFlags getUsageFlags() const noexcept  { return m_usage_flags; }
     vk::CommandBufferLevel getLevel() const noexcept { return m_level; }
     uint32_t getCount() const noexcept { return m_count; }
