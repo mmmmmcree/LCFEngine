07a592980d24b931dceff03793b3b84349dd943e
mmmmmcree <1349751341@qq.com>
2026-08-03 00:51:42 +0800

    enum_attributes_traits

--- utilities/core/include/concepts/enum_concept.h
+++ utilities/core/include/concepts/enum_concept.h
@@ -7,5 +7,5 @@ namespace lcf {
     concept enum_c = std::is_enum_v<T>;
 
     template <typename T>
-    concept enum_flags_c = std::is_enum_v<T> and is_enum_flags_v<T>;
+    concept enum_flags_c = enum_c<T> and is_enum_flags_v<T>;
 }
---
+++ utilities/enums/include/enums/enum_attributes_traits.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "concepts/enum_concept.h"
+#include "enums/enum_name.h"
+#include "enums/enum_values.h"
+#include <utility>
+
+namespace lcf {
+
+template <enum_c Enum>
+struct enum_common_attributes_traits
+{
+    template <Enum enum_value>
+    inline static constexpr auto index_of_v = std::to_underlying(enum_value);
+    template <Enum enum_value>
+    inline static auto name_of_v = enum_name<Enum>(enum_value);
+    inline static constexpr auto values_v = enum_values_v<Enum>;
+};
+
+template <enum_c Enum>
+struct enum_specialized_attributes_traits;
+
+} // namespace lcf
